456433d2c42fdc8c91a1ed40a4eea21a0778ee5c
Filatov ilya <filatov_26@mail.ru>
2021-10-06 14:00:12 +0300

    TimeZone

--- src/main/java/jm/task/core/jdbc/util/UtilHiber.java
+++ src/main/java/jm/task/core/jdbc/util/UtilHiber.java
@@ -17,7 +17,7 @@ public class UtilHiber {
                     Configuration configuration = new Configuration();
                     Properties settings = new Properties();
                     settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-                    settings.put(Environment.URL, "jdbc:mysql://localhost:3306/mybdtest?autoReconnect=true&useSSL=false");
+                    settings.put(Environment.URL, "jdbc:mysql://localhost:3306/mybdtest?serverTimezone=Europe/Moscow");
                     settings.put(Environment.USER, "root");
                     settings.put(Environment.PASS, "F1133363il@");
                     settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
